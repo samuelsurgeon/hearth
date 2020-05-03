45eafade6d8eb4889cf1d4dc2bfb8d2f014c4ec5
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-03 23:58:13 +1000

    Reorganise folders

--- hearth-functions/functions/index.js
+++ hearth-functions/functions/index.js
@@ -1,12 +1,5 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 const app = require('express')();
-const serviceAccount = require('./hearth-prototype-firebase-adminsdk-76upf-c8abbbd70c.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://console.firebase.google.com/project/hearth-prototype/database/firestore/data~2Fposts~2FHag1ji1lFQfXE9itFGCu'
-});
 
 const config = {
     apiKey: "AIzaSyA1RNEAAW7za0hhfG4berE3jBIu-nkE-hY",
@@ -21,8 +14,6 @@ const config = {
 
 const firebase = require('firebase');
 firebase.initializeApp(config);
-
-const db = admin.firestore();
 
 app.get('/posts', (req, res) => {
   db
---
+++ hearth-functions/functions/util/admin.js
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./hearth-prototype-firebase-adminsdk-76upf-c8abbbd70c.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://console.firebase.google.com/project/hearth-prototype/database/firestore/data~2Fposts~2FHag1ji1lFQfXE9itFGCu'
+});
+
+const db = admin.firestore();
+
+module.exports = { admin, db };
