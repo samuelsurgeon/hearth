9844e6a20594f8b199761a05aa91d7103a7c1d3e
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-04 17:23:35 +1000

    Website validation

--- hearth-functions/dbschema.js
+++ hearth-functions/dbschema.js
@@ -1,4 +1,16 @@
 let db = {
+  users: [
+    {
+      userId: 'dh23ggj5h32g543j5gf43',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2020-05-04T04:49:27.065Z',
+      imageUrl: 'image/fdsjkafd/fdsajfdk',
+      bio: 'Hello. My name is user, nice to meet you.',
+      website: 'https://user.com',
+      location: 'Sydney, NSW'
+    }
+  ]
   posts: [
     {
       userHandle: 'user',
--- hearth-functions/functions/index.js
+++ hearth-functions/functions/index.js
@@ -4,12 +4,13 @@ const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
 const { getAllPosts, postOnePost } = require('./handlers/posts');
-const { signup, login, uploadImage } = require('./handlers/users');
+const { signup, login, uploadImage, addUserDetails } = require('./handlers/users');
 
 app.get('/posts', getAllPosts);
 app.post('/post', FBAuth, postOnePost);
 app.post('/signup', signup);
 app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
 
 exports.api = functions.https.onRequest(app);
