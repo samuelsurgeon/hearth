9224922df40df6ff6bc68c958f883cd59d7bac8a
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-04 11:43:08 +1000

    Add image routes

--- hearth-functions/functions/index.js
+++ hearth-functions/functions/index.js
@@ -4,11 +4,12 @@ const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
 const { getAllPosts, postOnePost } = require('./handlers/posts');
-const { signup, login } = require('./handlers/users');
+const { signup, login, uploadImage } = require('./handlers/users');
 
 app.get('/posts', getAllPosts);
 app.post('/post', FBAuth, postOnePost);
 app.post('/signup', signup);
 app.post('/login', login);
+app.post('/user/image', uploadImage);
 
 exports.api = functions.https.onRequest(app);
