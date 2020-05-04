ad39de558518f3d9baaa321092bbb61cc470c782
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-04 17:29:46 +1000

    Add thing

--- hearth-functions/functions/handlers/users.js
+++ hearth-functions/functions/handlers/users.js
@@ -87,6 +87,14 @@ exports.login = (req, res) => {
 exports.addUserDetails = (req, res) => {
   let userDetails = reduceUserDetails(req.body);
   
+  db.doc(`/users/${req.user.handle}`).update(userDetails)
+    .then(() => {
+      return res.json({ message: 'Details added successfully' });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
 };
 
 exports.uploadImage = (req, res) => {
