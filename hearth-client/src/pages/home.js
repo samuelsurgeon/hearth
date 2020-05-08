4aac03ea07467ac601095040988d6b8d27eb794b
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-09 06:24:28 +1000

    Add axios to screams

--- hearth-client/src/pages/home.js
+++ hearth-client/src/pages/home.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Grid from '@material-ui/core/Grid';
 
 export default class home extends Component {
+  state = {
+    screams: null
+  }
+  
+  componentDidMount() {
+    axios.get('/screams')
+      .then(res => {
+        this.setState({
+          screams: res.data
+        })
+      })
+      .catch(err => console.error(err));
+  }
+
   render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map(scream => <p>{scream.body}</p>)
+    ) : <p>Loading...</p>
+
     return (
-      <Grid container>
+      <Grid container spacing={16}>
         <Grid item sm={8} xs={12}>
-          <p>Content...</p>
+          {recentScreamsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
           <p>Profile...</p>
