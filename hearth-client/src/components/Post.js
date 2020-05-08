31619bc8dbf5275914707423d6974156df5cb7af
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-09 07:32:55 +1000

    Add template for Post

--- hearth-client/src/components/Post.js
+++ hearth-client/src/components/Post.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
 const styles = {
   card: {
     display: 'flex'
@@ -9,12 +14,19 @@ const styles = {
 
 class Post extends Component {
   render() {
-    const { classes } = this.props;
+    const { classes, post : { body, createdAt, userImage, userHandle, postId, likeCount, commentCount } } = this.props;
 
     return (
-      <div>
-        
-      </div>
+      <Card>
+        <CardMedia
+        image={userImage}
+        title="Profile image" />
+        <CardContent>
+          <Typography variant="h5">{userHandle}</Typography>
+          <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
+          <Typography variant="body1">{body}</Typography>
+        </CardContent>
+      </Card>
     )
   }
 }
