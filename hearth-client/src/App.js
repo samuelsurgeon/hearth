c14d82a7b43c62c0892d6e42026b5d33727024db
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-09 09:51:36 +1000

    Add apple-touch-icon

--- hearth-client/src/App.js
+++ hearth-client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import './App.css';
 
 import NavBar from './components/Navbar';
 
