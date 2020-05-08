da5fd1047426a57ddabd9a08efc9b2c295208540
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-08 10:43:47 +1000

    Add page routes

--- hearth-client/src/App.js
+++ hearth-client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import home from './pages/Home';
+import login from './pages/Login';
+import signup from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hearth</h1>
+      <Router>
+        <Switch>
+          <Route path="/" component={home} />
+          <Route path="/login" component={login} />
+          <Route path="/signup" component={signup} />
+        </Switch>
+      </Router>
     </div>
   );
 }
