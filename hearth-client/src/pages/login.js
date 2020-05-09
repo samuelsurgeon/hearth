97828b87bacf5d370c1cdc7bff70449a747fb686
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-09 13:44:50 +1000

    Add authorisation logic

--- hearth-client/src/pages/login.js
+++ hearth-client/src/pages/login.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const styles = {
   form: {
     textAlign: 'center'
+  },
+  pageTitle: {
+    margin: '20px auto 20px auto'
+  },
+  textField: {
+    margin: '10px auto 10px auto'
+  },
+  button: {
+    marginTop: 20
+  },
+  customError: {
+    color: 'red',
+    fontSize: '0.8rem'
   }
 }
 
@@ -24,7 +39,28 @@ class login extends Component {
   }
 
   handleSubmit = event => {
-    console.log('Hi');
+    event.preventDefault();
+    this.setState({
+      loading: true
+    });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    axios.post('/login', userData)
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          loading: false
+        });
+        this.props.history.push('/');
+      })
+      .catch(err => {
+        this.setState({
+          errors: err.response.data,
+          loading: false
+        })
+      })
   }
 
   handleChange = event => {
@@ -35,6 +71,7 @@ class login extends Component {
 
   render() {
     const { classes } = this.props;
+    const { errors, loading } = this.state;
 
     return (
       <Grid container className={classes.form}>
@@ -48,9 +85,33 @@ class login extends Component {
               type="email" 
               label="E-mail" 
               className={classes.textField} 
+              helperText={errors.email}
+              error={errors.email ? true : false}
               value={this.state.email} 
-              onChange={this.handleChange()}
+              onChange={this.handleChange}
               fullWidth />
+            <TextField
+              id="password" 
+              name="password" 
+              type="password" 
+              label="Password" 
+              className={classes.textField} 
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password} 
+              onChange={this.handleChange}
+              fullWidth />
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <Button 
+              type="submit" 
+              variant="contained" 
+              color="primary" 
+              className={classes.button}>
+            Login</Button>
           </form>
         </Grid>
         <Grid item sm /> 
