effe6a5dc9188a93ed0d8b46ce3592d8c16c29dc
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-09 10:41:37 +1000

    Add logo

--- hearth-client/src/pages/login.js
+++ hearth-client/src/pages/login.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 const styles = {
-  
+  form: {
+    textAlign: 'center'
+  }
 }
 
 class login extends Component {
@@ -10,11 +16,19 @@ class login extends Component {
     const { classes } = this.props;
 
     return (
-      <div>
-        <h1>Login</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm /> 
+        <Grid item sm>
+          <Typography variant="h1">Logo</Typography>
+        </Grid>
+        <Grid item sm /> 
+      </Grid>
     );
   }
+}
+
+login.propTypes = {
+  classes: PropTypes.object.isRequired
 }
 
 export default withStyles(styles)(login);
