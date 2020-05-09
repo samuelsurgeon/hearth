cdab747eeb7c45fa5a3072336ff86911149c2eb8
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-09 11:12:56 +1000

    Add email textfield

--- hearth-client/src/pages/login.js
+++ hearth-client/src/pages/login.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
 const styles = {
   form: {
@@ -12,6 +13,26 @@ const styles = {
 }
 
 class login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      loading: false,
+      errors: {}
+    }
+  }
+
+  handleSubmit = event => {
+    console.log('Hi');
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
   render() {
     const { classes } = this.props;
 
@@ -19,7 +40,18 @@ class login extends Component {
       <Grid container className={classes.form}>
         <Grid item sm /> 
         <Grid item sm>
-          <Typography variant="h1">Logo</Typography>
+          <Typography variant="h2" className={classes.pageTitle}>Login</Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email" 
+              name="email" 
+              type="email" 
+              label="E-mail" 
+              className={classes.textField} 
+              value={this.state.email} 
+              onChange={this.handleChange()}
+              fullWidth />
+          </form>
         </Grid>
         <Grid item sm /> 
       </Grid>
