197eb3f3d6ef6dc1af1e2d2f51e2b0fde716f134
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-12 06:31:57 +1000

    Import reducers

--- hearth-client/src/pages/login.js
+++ hearth-client/src/pages/login.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import withStyles from '@material-ui/core/styles/withStyles';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 
 import Grid from '@material-ui/core/Grid';
+import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
+
+import { connect } from 'react-redux';
+import { loginUser } from '../redux/actions/userActions';
 
 const styles = theme => ({
   ...theme.spreadThis
@@ -30,6 +33,7 @@ class login extends Component {
       email: this.state.email,
       password: this.state.password,
     }
+    this.props.loginUser(userData, this.props.history);
   }
 
   handleChange = event => {
@@ -39,8 +43,8 @@ class login extends Component {
   }
 
   render() {
-    const { classes } = this.props;
-    const { errors, loading } = this.state;
+    const { classes, UI: { loading } } = this.props;
+    const { errors } = this.state;
 
     return (
       <Grid container className={classes.form}>
@@ -97,11 +101,20 @@ class login extends Component {
 }
 
 login.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  loginUser: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  user: state.user,
+  UI: state.UI
+});
+
+const mapActionsToProps = {
+  loginUser
 }
 
-export default withStyles(styles)(login);
-
-
-
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(login);
 
---
+++ hearth-client/src/redux/reducers/uiReducer.js
@@ -0,0 +1,3 @@
+import { SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from '../types';
+
+const initialState = 
