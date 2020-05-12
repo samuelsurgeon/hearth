b2930ffb27c76a5dd4b941faa0a89c8bdd08e961
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-13 09:20:38 +1000

    Set up basic EditDetails component

---
+++ hearth-client/src/components/EditDetails.js
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+import { connect } from 'react-redux';
+
+const styles = theme => ({
+  ...theme.spreadThis
+});
+
+class EditDetails extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default EditDetails;
