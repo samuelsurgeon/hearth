20c58f4f4ea365c56a8085ff8c2a95f9e6f5cd1c
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-13 06:46:06 +1000

    Rule up the basic JSX

--- hearth-client/src/components/Profile.js
+++ hearth-client/src/components/Profile.js
@@ -6,6 +6,54 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import Button from '@material-ui/core/Button';
 import { connect } from 'react-redux';
 
+const styles = {
+  paper: {
+    padding: 20
+  },
+  profile: {
+    '& .image-wrapper': {
+      textAlign: 'center',
+      position: 'relative',
+      '& button': {
+        position: 'absolute',
+        top: '80%',
+        left: '70%'
+      }
+    },
+    '& .profile-image': {
+      width: 200,
+      height: 200,
+      objectFit: 'cover',
+      maxWidth: '100%',
+      borderRadius: '50%'
+    },
+    '& .profile-details': {
+      textAlign: 'center',
+      '& span, svg': {
+        verticalAlign: 'middle'
+      },
+      '& a': {
+        color: theme.palette.primary.main
+      }
+    },
+    '& hr': {
+      border: 'none',
+      margin: '0 0 10px 0'
+    },
+    '& svg.button': {
+      '&:hover': {
+        cursor: 'pointer'
+      }
+    }
+  },
+  buttons: {
+    textAlign: 'center',
+    '& a': {
+      margin: '20px 10px'
+    }
+  }
+}
+
 class Profile extends Component {
   render() {
     const { 
@@ -14,8 +62,19 @@ class Profile extends Component {
         credentials: { handle, createdAt, imageUrl, bio, website, location }, 
         loading
       }
-    } = this.props; 
-    return <div />;
+    } = this.props;
+
+    let profileMarkup = !loading ? (authenticated ? (
+      <Paper className={classes.paper}>
+        <div className={classes.profile}>
+          <div className="profile-image">
+            <img src={imageUrl} alt="profile" />
+          </div>
+        </div>
+      </Paper>
+    ) : ()) : (<p>Loading...</p>);
+
+    return profileMarkup;
   }
 }
 
