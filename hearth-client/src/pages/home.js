6f1643efbe15e3516e1c30dd24b30093edd751ca
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-12 14:49:35 +1000

    Add profile.js

--- hearth-client/src/pages/home.js
+++ hearth-client/src/pages/home.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 
 import Post from '../components/Post';
+import Profile from '../components/Profile';
 
 export default class home extends Component {
   state = {
@@ -32,7 +33,7 @@ export default class home extends Component {
           {recentPostsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
-          <p>Profile...</p>
+          <Profile />
         </Grid>
       </Grid>
     );
