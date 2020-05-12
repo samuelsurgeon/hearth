2c3b3a6659af74bff4dfa0e06ddbe9787a09b679
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-12 14:54:44 +1000

    Add PropTypes and material-ui

---
+++ hearth-client/src/components/Profile.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import withStyles from '@material-ui/core/styles/withStyles';
+
+import Button from '@material-ui/core/Button';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Profile;
