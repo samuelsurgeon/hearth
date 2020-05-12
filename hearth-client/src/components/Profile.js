530e0c7844f286baab415db3936969ed1641a5ff
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-13 07:24:08 +1000

    Add buttons

--- hearth-client/src/components/Profile.js
+++ hearth-client/src/components/Profile.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router=dom';
+import dayjs from 'dayjs';
 
 import withStyles from '@material-ui/core/styles/withStyles';
 import Button from '@material-ui/core/Button';
@@ -90,10 +91,35 @@ class Profile extends Component {
                 <hr />
               </Fragment>
             )}
+          {website && (
+            <Fragment>
+              <LinkIcon color="primary" />
+              <a href={website} target="_blank" rel="noopener noreferrer">
+                {' '}{website}
+              </a>
+              <hr />
+            </Fragment>
+          )}
+          <CalendarToday color="primary" />{' '}
+          <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
           </div>
         </div>
       </Paper>
-    ) : ()) : (<p>Loading...</p>);
+    ) : (
+      <Paper className={classes.paper}>
+        <Typography variant="body2" align="center">
+          No profile found, please login again
+        </Typography>
+        <div className={classes.buttons}>
+          <Button variant="contained" color="primary" component={Link} to="/login">
+            Login
+          </Button>
+          <Button variant="contained" color="secondary" component={Link} to="/signup">
+            Signup
+          </Button>
+        </div>
+      </Paper>
+    )) : (<p>Loading...</p>);
 
     return profileMarkup;
   }
