c8245dda2e450ca7c61b7f42d372b463543a4c9e
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-13 07:57:52 +1000

    Add axios handler and dispatch for uploadImage

--- hearth-client/src/redux/actions/userActions.js
+++ hearth-client/src/redux/actions/userActions.js
@@ -54,6 +54,15 @@ export const getUserData = () => (dispatch) => {
     .catch(err => console.log(err));
 }
 
+const uploadImage = formData => dispatch => {
+  dispatch({ type: LOADING_USER });
+  axios.post('/user/image', formData)
+    .then(() => {
+      dispatch(getUserData());
+    })
+    .catch(err => console.log(err));
+}
+
 const setAuthorisationHeader = token => {
   const FBIdToken = `Bearer ${token}`;
   localStorage.setItem('FBIdToken', FBIdToken);
