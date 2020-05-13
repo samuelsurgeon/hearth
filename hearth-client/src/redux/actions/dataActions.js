2318a4f9b3cfbab285b6150b0f04c9f6e747105f
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-13 13:56:16 +1000

    Set up file template

---
+++ hearth-client/src/redux/actions/dataActions.js
@@ -0,0 +1,19 @@
+import { SET_POSTS, LOADING_DATA, LIKE_POST, UNLIKE_POST } from '../types';
+import axios from 'axios';
+
+export const getPosts = () => dispatch => {
+  dispatch({ type: LOADING_DATA });
+  axios.get('/posts')
+    .then(res => {
+      dispatch({
+        type: SET_POSTS,
+        payload: res.data
+      })
+    })
+    .catch(err => {
+      dispatch({
+        type: SET_POSTS,
+        payload: []
+      })
+    })
+};
