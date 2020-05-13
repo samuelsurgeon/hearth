4b9b6a71504d1252d16d93ff75eb85610413f57d
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-13 10:21:37 +1000

    Add textfields

--- hearth-client/src/components/EditDetails.js
+++ hearth-client/src/components/EditDetails.js
@@ -28,9 +28,21 @@ class EditDetails extends Component {
     open: false
   };
 
+  handleOpen = () => {
+    this.setState({ open: true });
+    mapUserDetailsToState(credentials);
+  }
+
+  handleClose = () => {
+    this.setState({ open: false });
+  }
+
   componentDidMount() {
     const { credentials } = this.props;
+    mapUserDetailsToState(credentials);
+  }
 
+  mapUserDetailsToState = credentials => {
     this.setState({
       bio: credentials.bio ? credentials.bio : '',
       website: credentials.website ? credentials.website : '',
@@ -39,9 +51,55 @@ class EditDetails extends Component {
   }
 
   render() {
+    const { classes } = this.props;
+    
     return (
       <Fragment>
-        
+        <Tooltip title="Edit details" placement="top">
+          <IconButton onClick={this.handleOpen} className={classes.button}>
+            <EditIcon color="primary" />
+          </IconButton>
+        </Tooltip>
+        <Dialog
+        open={this.state.open}
+        onClose={this.handleClose}
+        fullWidth
+        maxWidth="sm">
+          <DialogTitle>Edit your details</DialogTitle> 
+          <DialogContent>
+            <form>
+              <Textfield
+                name="bio"
+                type="text"
+                label="Bio"
+                multiline
+                rows="3"
+                placeholders="A short bio about yourself"
+                className={classes.textField}
+                value={this.state.bio}
+                onChange={this.handleChange}
+                fullWidth />
+              <Textfield
+                name="website"
+                type="text"
+                label="Website"
+                placeholders="Your personal website"
+                className={classes.textField}
+                value={this.state.website}
+                onChange={this.handleChange}
+                fullWidth />
+              <Textfield
+                name="location"
+                type="text"
+                label="Location"
+                placeholders="Where you live"
+                className={classes.textField}
+                value={this.state.location}
+                onChange={this.handleChange}
+                fullWidth />
+            </form>
+          </DialogContent>
+        </Dialog>
       </Fragment>
     )
   }
