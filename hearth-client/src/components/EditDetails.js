313ff19311794b6530a7028fbb5003e13a0c7f49
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-13 10:03:46 +1000

    Add MUI stuff

--- hearth-client/src/components/EditDetails.js
+++ hearth-client/src/components/EditDetails.js
@@ -3,18 +3,57 @@ import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 
 import { connect } from 'react-redux';
+import { editUserData } from '../redux/actions/userActions';
+
+import Button from '@material-ui/core/Button';
+import Textfield from '@material-ui/core/Textfield';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/core/iconsEdit';
 
 const styles = theme => ({
   ...theme.spreadThis
 });
 
 class EditDetails extends Component {
+  state = {
+    bio: '',
+    website: '',
+    location: '',
+    open: false
+  };
+
+  componentDidMount() {
+    const { credentials } = this.props;
+
+    this.setState({
+      bio: credentials.bio ? credentials.bio : '',
+      website: credentials.website ? credentials.website : '',
+      location: credentials.location ? credentials.location : ''
+    });
+  }
+
   render() {
     return (
-      <div>
-      </div>
+      <Fragment>
+        
+      </Fragment>
     )
   }
 }
 
-export default EditDetails;
+EditDetails.propTypes = {
+  editUserDetails: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  credentials: state.user.credentials
+});
+
+export default connect(mapStateToProps, { editUserDetails })(withStyles(styles)(EditDetails));
--- hearth-client/src/redux/actions/userActions.js
+++ hearth-client/src/redux/actions/userActions.js
@@ -63,6 +63,15 @@ export const uploadImage = formData => dispatch => {
     .catch(err => console.log(err));
 }
 
+export const editUserDetails = userDetails => dispatch => {
+  dispatch({ type: LOADING_USER });
+  axios.post('/user', userDetails)
+    .then(() => {
+      dispatch(getUserData());
+    })
+    .catch(err => console.log(err));
+}
+
 const setAuthorisationHeader = token => {
   const FBIdToken = `Bearer ${token}`;
   localStorage.setItem('FBIdToken', FBIdToken);
