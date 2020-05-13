77f99f8434ae8bbb6aafbd7bc05e963bd7bf10b1
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-13 14:11:23 +1000

    Add LIKE and UNLIKE functionality

--- hearth-client/src/redux/actions/dataActions.js
+++ hearth-client/src/redux/actions/dataActions.js
@@ -17,3 +17,25 @@ export const getPosts = () => dispatch => {
       })
     })
 };
+
+export const likePost = postId => dispatch => {
+  axios.get(`/post/${postId}/like`)
+    .then(res => {
+      dispatch({
+        type: LIKE_POST,
+        payload: res.data
+      })
+    })
+    .catch(err => console.log(err));
+}
+
+export const unlikePost = postId => dispatch => {
+  axios.get(`/post/${postId}/unlike`)
+    .then(res => {
+      dispatch({
+        type: UNLIKE_POST,
+        payload: res.data
+      })
+    })
+    .catch(err => console.log(err));
+}
