a4e929a6d61575a4b057f6baea9772d3de540280
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-13 13:59:34 +1000

    Add more types

--- hearth-client/src/redux/types.js
+++ hearth-client/src/redux/types.js
@@ -6,4 +6,9 @@ export const LOADING_USER = 'LOADING_USER';
 export const SET_ERRORS = 'SET_ERRORS';
 export const LOADING_UI = 'LOADING_UI';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const LOADING_DATA = 'LOADING_DATA';
 
+export const SET_POSTS = 'SET_POSTS';
+export const SET_POST = 'SET_POST';
+export const LIKE_POST = 'SET_POST';
+export const UNLIKE_POST = 'UNLIKE_POST';
