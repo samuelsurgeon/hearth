8645460fe76fd965f02d9200a5b8d7f9b6ed983f
Samuel Surgeon <ssurgeon00@icloud.com>
2020-05-15 14:18:12 +1000

    Attempt fix of posts undefined

--- hearth-client/src/redux/actions/dataActions.js
+++ hearth-client/src/redux/actions/dataActions.js
@@ -111,6 +111,23 @@ export const deletePost = postId => dispatch => {
     .catch(err => console.log(err));
 }
 
+export const getUserData = userHandle => dispatch => {
+  dispatch({ type: LOADING_DATA });
+  axios.get(`/user/${userHandle}`)
+    .then(res => {
+      dispatch({
+        type: SET_POSTS,
+        payload: res.data.posts
+      });
+    })
+    .catch(() => {
+      dispatch({
+        type: SET_POSTS,
+        payload: null
+      })
+    })
+}
+
 export const clearErrors = () => dispatch => {
   dispatch({ type: CLEAR_ERRORS });
 }
